3d16aedb2148e57cd7954a3214cf83ac8421d5e6
Alok Regmi <sagar.r.alok@gmail.com>
2021-09-19 15:15:15 +0545

    Code to read config yaml

--- src/Feature_Importance.py
+++ src/Feature_Importance.py
@@ -1,1 +1,22 @@
+# -*- coding: utf-8 -*-
 # Alok's python Feature Importance Script here
+
+import yaml
+
+
+def read_config(fname="src/config.yaml"):
+    """ Function to read and return config params from yaml file
+
+    Args:
+        fname (str) : File name for yaml file
+    Returns:
+        dict : Dictionary with parameters from yaml file
+
+    """
+    with open(fname, "r") as fs:
+        try:
+            return yaml.safe_load(fs)
+        except yaml.YAMLError as exc:
+            print(exc)
+            return
+
