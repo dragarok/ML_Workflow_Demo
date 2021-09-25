4083c75fcbc8f10c5415a3e7bcea7ffd7e4b7a72
Alok Regmi <sagar.r.alok@gmail.com>
2021-09-25 15:01:56 +0545

    Tests for running pipeline by pulling github

--- src/cloud_runner.py
+++ src/cloud_runner.py
@@ -10,17 +10,18 @@ import sys
 
 gcp_bucket = 'tfc-cml'
 
-print("Moved file to src")
-tfc.run(
-    entry_point='../../../src/tfc_cloud.py',
-    requirements_txt='../../../src/requirements-tfcloud.txt',
-    chief_config=tfc.MachineConfig(
-            cpu_cores=8,
-            memory=30,
-            accelerator_type=tfc.AcceleratorType.NVIDIA_TESLA_T4,
-            accelerator_count=1),
-    docker_image_bucket_name=gcp_bucket,
-)
+# tfc.run(
+#     entry_point='../../../src/tfc_cloud.py',
+#     requirements_txt='../../../src/requirements-tfcloud.txt',
+#     chief_config=tfc.MachineConfig(
+#             cpu_cores=8,
+#             memory=30,
+#             accelerator_type=tfc.AcceleratorType.NVIDIA_TESLA_T4,
+#             accelerator_count=1),
+#     docker_image_bucket_name=gcp_bucket,
+# )
+
+# Fetch repo from github
 token = ''
 branch_name = ''
 github_username = ''
@@ -28,20 +29,30 @@ repo_main_url = ''
 # token = os.getenv('GIT_TOKEN')
 # branch_name = os.getenv('BRANCH_NAME')
 git_url = "https://" + github_username + ":" + token + "@" + repo_main_url
+print(git_url)
 repo = Repo.clone_from(git_url, 'cloned_repo')
 repo.git.checkout(branch_name)
 print("Cloned the repo")
 # subprocess.run(['pyenv', 'activate dvc_tfc'])
-# ret = subprocess.run([command], capture_output=True, shell=True)
+pull_dvc_cmd = 'cd cloned_repo; dvc pull --run-cache'
+ret = subprocess.run([pull_dvc_cmd], capture_output=True, shell=True)
+print(ret)
+print("\nPulled the data")
+
+# Call pipeline reproduce to run the model training
+# model_path = run_model_training()
+add_model_dvc_cmd = 'cd cloned_repo; dvc repro -R ContinuousML'
+ret = subprocess.run([add_model_dvc_cmd], capture_output=True, shell=True)
+print(ret)
+push_model_dvc_cmd = 'cd cloned_repo; dvc push'
+ret = subprocess.run([push_model_dvc_cmd], capture_output=True, shell=True)
+print(ret)
+# convert_model_cmd = "python -m tf2onnx.convert --saved-model " + model_path +  " --output model.onnx"
+# # process = subprocess.Popen(pull_cmd_dvc, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+# # for line in iter(process.stdout.readline, b''):
+# #     sys.stdout.write(line.decode(sys.stdout.encoding))
+# ret = subprocess.run([convert_model_cmd], capture_output=True, shell=True)
 # print(ret)
-pull_cmd_dvc = 'cd cloned_repo; dvc pull --run-cache'
-process = subprocess.Popen(pull_cmd_dvc, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
-for line in iter(process.stdout.readline, b''):
-    sys.stdout.write(line.decode(sys.stdout.encoding))
-print("Pulled the data")
-run_model_training()
-
-
 
 # TODO Watching job status updates
 # projectName = 'your_project_name'
--- src/tfc_cloud.py
+++ src/tfc_cloud.py
@@ -7,67 +7,80 @@ import pandas as pd
 import datetime
 import os
 
-gcp_bucket = 'tfc-cml'
-BATCH_SIZE = 32
+def run_model_training():
+    """Run model training using tensorflow
 
-df = pd.read_csv("Full_Features.csv")
+    # TODO Refactor the function
+    Args:
 
-features_df = df.drop(['Label', 'Bar'], axis=1)
-labels_df = df['Label']
+    Output:
+        None"""
+    gcp_bucket = 'tfc-cml'
+    BATCH_SIZE = 32
 
-k_best_cols_idx = (
-    SelectKBest(chi2, k=300)
-    .fit(features_df, labels_df)).get_support(indices=True)
-f_cols_list = list(features_df.columns)
-k_best_cols = [f_cols_list[i] for i in k_best_cols_idx]
+    df = pd.read_csv("cloned_repo/ContinuousML/NQ_DR_4_10_20_Ideal_27/1_Label_And_Feature_Workflow/Full_Features.csv")
 
-k_best_features_df = features_df[k_best_cols]
+    features_df = df.drop(['Label', 'Bar'], axis=1)
+    labels_df = df['Label']
 
-X_train, X_test, y_train, y_test = train_test_split(k_best_features_df, labels_df,
-                                                    test_size=0.2,
-                                                    random_state=42)
-# FIXME Error with dataset sharding
-train_data = tf.data.Dataset.from_tensor_slices((X_train, y_train))
-test_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
+    k_best_cols_idx = (
+        SelectKBest(chi2, k=300)
+        .fit(features_df, labels_df)).get_support(indices=True)
+    f_cols_list = list(features_df.columns)
+    k_best_cols = [f_cols_list[i] for i in k_best_cols_idx]
 
-train_dataset = train_data.batch(BATCH_SIZE)
-test_dataset = test_data.batch(BATCH_SIZE)
+    k_best_features_df = features_df[k_best_cols]
 
-model = tf.keras.Sequential([
-    tf.keras.layers.Dense(512, activation='relu'),
-    tf.keras.layers.Dense(256, activation='relu'),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(14, activation='softmax')
-])
+    X_train, X_test, y_train, y_test = train_test_split(k_best_features_df, labels_df,
+                                                        test_size=0.2,
+                                                        random_state=42)
+    # FIXME Error with dataset sharding
+    train_data = tf.data.Dataset.from_tensor_slices((X_train, y_train))
+    test_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
 
-checkpoint_path = os.path.join("gs://", gcp_bucket, "feat-sel-check", "save_at_{epoch}")
+    train_dataset = train_data.batch(BATCH_SIZE)
+    test_dataset = test_data.batch(BATCH_SIZE)
 
-tensorboard_path = os.path.join(  # Timestamp included to enable timeseries graphs
-    "gs://", gcp_bucket, "logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
-)
+    model = tf.keras.Sequential([
+        tf.keras.layers.Dense(512, activation='relu'),
+        tf.keras.layers.Dense(256, activation='relu'),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(64, activation='relu'),
+        tf.keras.layers.Dense(14, activation='softmax')
+    ])
 
-callbacks = [
-    # TensorBoard will store logs for each epoch and graph performance for us.
-    # tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path, histogram_freq=1),
-    # ModelCheckpoint will save models after each epoch for retrieval later.
-    tf.keras.callbacks.ModelCheckpoint(checkpoint_path),
-    # EarlyStopping will terminate training when val_loss ceases to improve.
-    # tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
-]
+    checkpoint_path = os.path.join("gs://", gcp_bucket, "feat-sel-check", "save_at_{epoch}")
 
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer=tf.keras.optimizers.Adam(),
-              metrics=['accuracy'])
+    tensorboard_path = os.path.join(  # Timestamp included to enable timeseries graphs
+        "gs://", gcp_bucket, "logs", datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+    )
 
-model.fit(train_dataset, callbacks=callbacks, epochs=1)
+    callbacks = [
+        # TensorBoard will store logs for each epoch and graph performance for us.
+        # tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path, histogram_freq=1),
+        # ModelCheckpoint will save models after each epoch for retrieval later.
+        tf.keras.callbacks.ModelCheckpoint(checkpoint_path),
+        # EarlyStopping will terminate training when val_loss ceases to improve.
+        # tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
+    ]
 
-MODEL_PATH = "keras-feat-sel"
-SAVE_PATH = os.path.join("gs://", gcp_bucket, MODEL_PATH)
-model.save(SAVE_PATH)
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer=tf.keras.optimizers.Adam(),
+                  metrics=['accuracy'])
 
-model = tf.keras.models.load_model(SAVE_PATH)
+    # model.fit(train_dataset, callbacks=callbacks, epochs=1)
+    model.fit(train_dataset, epochs=1)
 
-print(model.evaluate(test_dataset, batch_size=32))
+    MODEL_PATH = "cloned_repo/ContinuousML/NQ_DR_4_10_20_Ideal_27/2_Training_Workflow/keras-model.h5"
+    # SAVE_PATH = os.path.join("gs://", gcp_bucket, MODEL_PATH)
+    SAVE_PATH = MODEL_PATH
+    model.save(SAVE_PATH)
 
-print("Done fitting the model")
+    model = tf.keras.models.load_model(SAVE_PATH)
+
+    print(model.evaluate(test_dataset, batch_size=32))
+    print("Done evaluating the model")
+    return "ContinuousML/NQ_DR_4_10_20_Ideal_27/2_Training_Workflow/keras-model.h5"
+
+if __name__ == "__main__":
+    run_model_training()
