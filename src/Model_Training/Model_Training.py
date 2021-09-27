d50db0d8b767b738aafdd71504357c2b256a3360
Alok Regmi <sagar.r.alok@gmail.com>
2021-09-27 12:05:52 +0545

    Add requirements to each stage

--- src/Model_Training/Model_Training.py
+++ src/Model_Training/Model_Training.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 import tensorflow as tf
-from sklearn.feature_selection import SelectKBest, chi2, f_classif
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import datetime
