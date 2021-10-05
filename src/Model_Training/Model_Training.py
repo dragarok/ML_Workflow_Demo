30c32bb614e8b8592000c97ffb39f524bd5b45f3
Alok Regmi <sagar.r.alok@gmail.com>
2021-10-05 12:08:26 +0545

    f1score as callback rather than batch metric

--- src/Model_Training/Model_Training.py
+++ src/Model_Training/Model_Training.py
@@ -5,7 +5,8 @@ from tensorflow.keras import backend as K
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
 import tensorflow_addons as tfa
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report, confusion_matrix, f1_score, precision_score, recall_score
+from tensorflow.keras.callbacks import Callback
 import pandas as pd
 import numpy as np
 import datetime
@@ -16,6 +17,29 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 from dvclive.keras import DvcLiveCallback
+import talos as ta
+
+class Metrics(tf.keras.callbacks.Callback):
+    def __init__(self, valid_data):
+        super(Metrics, self).__init__()
+        self.validation_data = valid_data
+
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        val_predict = np.argmax(self.model.predict(self.validation_data[0]), -1)
+        val_targ = self.validation_data[1]
+        if len(val_targ.shape) == 2 and val_targ.shape[1] != 1:
+            val_targ = np.argmax(val_targ, -1)
+
+        _val_f1 = f1_score(val_targ, val_predict, average='macro')
+        _val_recall = recall_score(val_targ, val_predict, average='macro')
+        _val_precision = precision_score(val_targ, val_predict, average='macro')
+
+        logs['val_f1'] = _val_f1
+        logs['val_recall'] = _val_recall
+        logs['val_precision'] = _val_precision
+        print(" — val_f1: %f — val_precision: %f — val_recall: %f" % (_val_f1, _val_precision, _val_recall))
+        return
 
 
 def run_model_training():
@@ -27,9 +51,10 @@ def run_model_training():
     Output:
         None"""
 
+
     with open("params.yaml", 'r') as fd:
         params = yaml.safe_load(fd)
-
+    MODEL_NAME = 'keras_model'
     SEED = params['seed']
     GCP_BUCKET = params['gcp_bucket']
     TEST_SIZE = params['test_size']
@@ -50,8 +75,6 @@ def run_model_training():
     # Since labels are from 0 to max value
     N_LABELS = max(labels_df.unique()) + 1
     N_LABELS = N_LABELS.item()
-
-    MODEL_NAME = 'keras_model'
 
     X_train, X_test, y_train, y_test = train_test_split(features_df, labels_df,
                                                         test_size=TEST_SIZE,
@@ -78,20 +101,25 @@ def run_model_training():
         # TensorBoard will store logs for each epoch and graph performance for us.
         tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path, histogram_freq=1),
         # ModelCheckpoint will save models after each epoch for retrieval later.
-        tf.keras.callbacks.ModelCheckpoint(checkpoint_path, verbose=1, monitor='val_fbeta_score', save_best_only=True, mode='max'),
+        tf.keras.callbacks.ModelCheckpoint(checkpoint_path, verbose=1, monitor='val_f1', save_best_only=True, mode='max'),
         # EarlyStopping will terminate training when val_loss ceases to improve.
         # tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
-        DvcLiveCallback(model_file="saved_model.h5")
+        DvcLiveCallback(model_file="saved_model.h5"),
+        # TODO Looks like it fixes the metric we have been looking for
+        Metrics(valid_data=(X_test, y_test))
         ]
 
     model.compile(loss='sparse_categorical_crossentropy',
-                  optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
-                  # TODO More look on what the metric should be
-                  metrics=[tfa.metrics.FBetaScore(num_classes=N_LABELS, average="micro", threshold=0.9)])
+                  optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),)
+                  # TODO Test with more data for F1score as well as fbeta score
+                  # metrics=[tfa.metrics.FBetaScore(num_classes=N_LABELS, average="micro", threshold=0.9), f1])
 
     # model.fit(train_data, callbacks=callbacks, epochs=1)
     history = model.fit(train_data, epochs=EPOCHS, validation_data=val_data, callbacks=callbacks)
     print("Model training done")
+    print(history.history)
+    print("\n\n\n")
+
 
     losses_df = pd.DataFrame(columns=['epoch', 'loss', 'fbeta_score', 'val_loss', 'val_fbeta_score'])
     for i in range(EPOCHS):
@@ -117,6 +145,7 @@ def run_model_training():
 
     # Confusion Matrix to be plotted by dvc
     y_pred = np.argmax(model.predict(X_test), axis=1)
+    print(classification_report(y_test, y_pred))
     confusion_df = pd.DataFrame({'actual': y_test,
                                   'predicted': y_pred})
     confusion_df.to_csv("classes.csv", index=False)
