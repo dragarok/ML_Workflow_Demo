ff1736ba38488180b42df1fc2bd13d498e0ffaee
Alok Regmi <sagar.r.alok@gmail.com>
2021-10-06 16:04:28 +0545

    Working onnx inference

--- src/Model_Training/onnx_inference.py
+++ src/Model_Training/onnx_inference.py
@@ -9,20 +9,16 @@ model = 'keras_model.onnx'
 
 try:
     input_row = sys.argv[1]
-    print(input_row)
 except ValueError as e:
     print(e)
 
 a = input_row.split(',')
-input_array = np.array(a).astype('float32')
-session = onnxruntime.InferenceSession(model, None)
-input_name = session.get_inputs()[0]
-output_name = session.get_outputs()[0]
-print(input_name)
-print(output_name)
-print(input_name.shape)
-# input_array = input_array.reshape((1,input_name.shape[1]))
-print(input_array.shape)
+session = onnxruntime.InferenceSession(model)
+onnx_input = session.get_inputs()[0]
+input_name = onnx_input.name
+output_name = session.get_outputs()[0].name
+input_array = np.array(a).reshape(1, onnx_input.shape[1]).astype(np.float32)
 result = session.run([output_name], {input_name: input_array})
+print(result)
 prediction = int(np.argmax(np.array(result).squeeze(), axis=0))
 print(prediction)
