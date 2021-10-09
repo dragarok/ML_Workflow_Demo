47d172817b470408acd33ed38497e291e2c59caf
Alok Regmi <36977610+dragarok@users.noreply.github.com>
2021-10-09 18:22:33 +0545

    Hyperparameter tuning workflow with model

--- src/Model_Training/Model_Training.py
+++ src/Model_Training/Model_Training.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
 
-
-from imblearn.over_sampling import SMOTE
-from imblearn.under_sampling import RandomUnderSampler
-from imblearn.pipeline import Pipeline
-from imblearn.over_sampling import SVMSMOTE
-from imblearn.combine import SMOTETomek
-
 import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
 import tensorflow_addons as tfa
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import (
-    classification_report,
-    confusion_matrix,
-    f1_score,
-    precision_score,
-    recall_score,
-)
+from sklearn.metrics import classification_report, confusion_matrix, f1_score, precision_score, recall_score
 from tensorflow.keras.callbacks import Callback
 import pandas as pd
 import numpy as np
@@ -30,7 +17,51 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import json
 from dvclive.keras import DvcLiveCallback
-from sklearn.utils.class_weight import compute_class_weight
+import optuna
+import nvgpu
+from optuna.integration.tfkeras import TFKerasPruningCallback
+from optuna.trial import TrialState
+from optuna.visualization import plot_parallel_coordinate
+from optuna.visualization import plot_param_importances
+from imblearn.over_sampling import SMOTE
+
+
+with open("params.yaml", "r") as fd:
+    params = yaml.safe_load(fd)
+
+SEED = params["seed"]
+MODEL_NAME = params["model_name"]
+TEST_SIZE = params["test_size"]
+
+if params["mode"] == "hyp_tuning":
+    core_params = params["hyp_tuning"]
+else:
+    core_params = params["train"]
+
+EPOCHS = core_params["epochs"]
+LEARNING_RATE = core_params["learning_rate"]
+BATCH_SIZE = core_params["batch_size"]
+ACTIVATION = core_params["activation"]
+OPTIMIZER = core_params['optimizer']
+LAYERS = core_params["dense_layers"]
+
+EVAL_BATCH_SIZE = params["eval"]["batch_size"]
+
+
+df = pd.read_csv("Reduced_Features.csv")
+features_df = df.drop(['Label'], axis=1)
+labels_df = df['Label']
+
+N_FEATURES = len(features_df.columns)
+# Since labels are from 0 to max value
+N_LABELS = max(labels_df.unique()) + 1
+N_LABELS = N_LABELS.item()
+
+oversample = SMOTE()
+X, y = oversample.fit_resample(features_df, labels_df)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, stratify=y, test_size=TEST_SIZE, random_state=SEED
+)
 
 
 class Metrics(tf.keras.callbacks.Callback):
@@ -45,150 +76,137 @@ class Metrics(tf.keras.callbacks.Callback):
         if len(val_targ.shape) == 2 and val_targ.shape[1] != 1:
             val_targ = np.argmax(val_targ, -1)
 
-        _val_f1 = f1_score(val_targ, val_predict, average="weighted")
-        _val_recall = recall_score(val_targ, val_predict, average="weighted")
-        _val_precision = precision_score(val_targ, val_predict, average="weighted")
+        _val_f1 = f1_score(val_targ, val_predict, average='weighted', zero_division=0)
+        _val_recall = recall_score(val_targ, val_predict, average='weighted', zero_division=0)
+        _val_precision = precision_score(val_targ, val_predict, average='weighted', zero_division=0)
 
-        logs["val_f1"] = _val_f1
-        logs["val_recall"] = _val_recall
-        logs["val_precision"] = _val_precision
-        print(
-            " — val_f1: %f — val_precision: %f — val_recall: %f"
-            % (_val_f1, _val_precision, _val_recall)
-        )
+        logs['val_f1'] = _val_f1
+        logs['val_recall'] = _val_recall
+        logs['val_precision'] = _val_precision
+        print(" — val_f1: %f — val_precision: %f — val_recall: %f" % (_val_f1, _val_precision, _val_recall))
         return
 
 
-def run_model_training():
-    """Run model training using tensorflow
 
-    # TODO Refactor the function
-    Args:
+def create_model(trial):
+    """ Create model using individual dense layers"""
 
-    Output:
-        None"""
+    ACTIVATION = core_params["activation"]
+    N_LAYERS = core_params["n_layers"]
+    LEARNING_RATE = core_params["learning_rate"]
+    DROPOUT_RATE = core_params["dropout"]
 
-    with open("params.yaml", "r") as fd:
-        params = yaml.safe_load(fd)
-    MODEL_NAME = "keras_model"
-    SEED = params["seed"]
-    GCP_BUCKET = params["gcp_bucket"]
-    TEST_SIZE = params["test_size"]
+    ACTIVATION = trial.suggest_categorical("activation", ACTIVATION)
+    N_LAYERS = trial.suggest_int('n_layers', 1, N_LAYERS)
+    LEARNING_RATE = trial.suggest_float("learning_rate", LEARNING_RATE[0], LEARNING_RATE[1], log=True)
+    UNITS = trial.suggest_categorical("layers", LAYERS)
 
-    EPOCHS = params["train"]["epochs"]
-    BATCH_SIZE = params["train"]["batch_size"]
-    ACTIVATION = params["train"]["activation"]
-    LAYERS = params["train"]["fc_layers"]
-    LEARNING_RATE = params["train"]["learning_rate"]
+    model = tf.keras.Sequential()
+    model.add(tf.keras.layers.Flatten())
+    for i in range(N_LAYERS):
+        NUM_HIDDEN = trial.suggest_int("n_units_l{}".format(i), 4, 128, log=True)
+        model.add(tf.keras.layers.Dense(NUM_HIDDEN, activation=ACTIVATION))
+        DROPOUT = trial.suggest_discrete_uniform("dropout_l{}".format(i), DROPOUT_RATE[0], DROPOUT_RATE[1], DROPOUT_RATE[2])
+        model.add(tf.keras.layers.Dropout(rate=DROPOUT))
+    model.add(tf.keras.layers.Dense(N_LABELS, activation='softmax'))
 
-    EVAL_BATCH_SIZE = params["eval"]["batch_size"]
-
-    df = pd.read_csv("Reduced_Features.csv")
-    features_df = df.drop(["Label"], axis=1)
-    labels_df = df["Label"]
-
-    N_FEATURES = len(features_df.columns)
-    # Since labels are from 0 to max value
-    N_LABELS = max(labels_df.unique()) + 1
-    N_LABELS = N_LABELS.item()
-
-    # Oversample smaller classes
-    oversample = SMOTE()
-    X, y = oversample.fit_resample(features_df, labels_df)
-
-    # sm = SMOTE()
-    # X, y = sm.fit_resample(features_df, labels_df)
-
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, stratify=y, test_size=TEST_SIZE, random_state=SEED
+    # Compile model.
+    model.compile(
+        optimizer=tf.keras.optimizers.Adam(learning_rate=LEARNING_RATE),
+        loss="sparse_categorical_crossentropy",
     )
+    return model
 
-    # X_train, X_test, y_train, y_test = train_test_split(
-    #     features_df, labels_df, stratify=labels_df, test_size=TEST_SIZE, random_state=SEED
-    # )
 
-    class_labels = np.unique(y_train)
-    class_weights = compute_class_weight(
-        class_weight="balanced", classes=class_labels, y=y_train
-    )
-    class_weight = dict(zip(class_labels, class_weights))
-    for i in range(N_LABELS):
-        if i not in class_weight.keys():
-            class_weight[i] = 1
+def objective(trial, return_model=False):
+    """Create an objective for optuna"""
 
-    print(class_weight)
+    # Clear clutter from previous TensorFlow graphs.
+    tf.keras.backend.clear_session()
+    if 'train_dataset' in globals():
+        del train_dataset
+        del train_data
+        del val_data
+
+    # Parameters
+    EPOCHS = core_params["epochs"]
+    BATCH_SIZE = core_params["batch_size"]
+    OPTIMIZER = core_params['optimizer']
+
+    # Hyperparameters to be tuned by Optuna.
+    assert type(LEARNING_RATE) == list
+    EPOCHS = trial.suggest_categorical("epochs", EPOCHS)
+    BATCH_SIZE = trial.suggest_categorical("batch_size", BATCH_SIZE)
+
+    # Metrics to be monitored by Optuna.
+    monitor = "val_f1"
+
     train_dataset = tf.data.Dataset.from_tensor_slices((X_train, y_train))
     train_data = train_dataset.shuffle(len(X_train)).batch(BATCH_SIZE)
     train_data = train_data.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
     val_data = tf.data.Dataset.from_tensor_slices((X_test, y_test))
     val_data = val_data.batch(EVAL_BATCH_SIZE)
-    # TODO Optimization using prefetch
 
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Flatten())
-    model.add(tf.keras.layers.Dense(units=512, activation="relu"))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
-    model.add(tf.keras.layers.Dense(units=256, activation="relu"))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
-    model.add(tf.keras.layers.Dense(units=64, activation="relu"))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
-    model.add(tf.keras.layers.Dense(units=256, activation="relu"))
-    model.add(tf.keras.layers.Dropout(rate=0.3))
-    model.add(tf.keras.layers.Dense(units=N_LABELS, activation="sigmoid"))
-
-    checkpoint_path = os.path.join("feat-sel-check", "save_at_{epoch}")
-    tensorboard_path = "logs"
+    model = create_model(trial)
 
     callbacks = [
-        # TensorBoard will store logs for each epoch and graph performance for us.
-        # tf.keras.callbacks.TensorBoard(log_dir=tensorboard_path, histogram_freq=1),
-        # ModelCheckpoint will save models after each epoch for retrieval later.
-        tf.keras.callbacks.ModelCheckpoint(
-            checkpoint_path,
-            verbose=1,
-            monitor="val_loss",
-            save_best_only=True,
-        ),
-        # EarlyStopping will terminate training when val_loss ceases to improve.
-        # tf.keras.callbacks.EarlyStopping(monitor="val_loss", patience=3),
-        # DvcLiveCallback(model_file="saved_model.h5"),
-        # TODO Looks like it fixes the metric we have been looking for
+        tf.keras.callbacks.EarlyStopping(patience=3),
         Metrics(valid_data=(X_test, y_test)),
+        TFKerasPruningCallback(trial, monitor),
     ]
 
-    model.compile(
-        loss="sparse_categorical_crossentropy",
-        optimizer=tfa.optimizers.AdamW(learning_rate=1e-3, weight_decay=1e-5),
-    )
-    # TODO Test with more data for F1score as well as fbeta score
-    # metrics=[tfa.metrics.FBetaScore(num_classes=N_LABELS, average="micro", threshold=0.9), f1])
-
-    # model.fit(train_data, callbacks=callbacks, epochs=1)
+    # Train model.
     history = model.fit(
         train_data,
         epochs=EPOCHS,
         validation_data=val_data,
         callbacks=callbacks,
-        # class_weight=class_weight
     )
-    print("Model training done")
-    print(history.history)
-    print("\n\n\n")
+    if return_model:
+        return history.history[monitor][-1], model, history
+    else:
+        return history.history[monitor][-1]
 
-    # Confusion Matrix to be plotted by dvc
-    y_pred = np.argmax(model.predict(X_test), axis=1)
-    print(classification_report(y_test, y_pred))
-    confusion_df = pd.DataFrame({"actual": y_test, "predicted": y_pred})
-    confusion_df.to_csv("classes.csv", index=False)
 
-    # Convert model to onnx
-    spec = (tf.TensorSpec((None, N_FEATURES), tf.float32, name="input"),)
-    output_path = MODEL_NAME + ".onnx"
-    model_proto, _ = tf2onnx.convert.from_keras(
-        model, opset=13, output_path=output_path, input_signature=spec
-    )
-    print("Converted and saved the model to ONNX file")
+def show_result(study):
+
+    pruned_trials = study.get_trials(deepcopy=False, states=[TrialState.PRUNED])
+    complete_trials = study.get_trials(deepcopy=False, states=[TrialState.COMPLETE])
+
+    print("Study statistics: ")
+    print("  Number of finished trials: ", len(study.trials))
+    print("  Number of pruned trials: ", len(pruned_trials))
+    print("  Number of complete trials: ", len(complete_trials))
+
+    print("Best trial:")
+    trial = study.best_trial
+
+    print("  Value: ", trial.value)
+
+    print("  Params: ")
+    for key, value in trial.params.items():
+        print("    {}: {}".format(key, value))
 
 
 if __name__ == "__main__":
-    run_model_training()
+
+    study = optuna.create_study(
+        direction="maximize", pruner=optuna.pruners.MedianPruner(n_startup_trials=2)
+    )
+
+    study.optimize(objective, n_trials=5, timeout=800)
+
+    show_result(study)
+
+    fig1 = plot_parallel_coordinate(study)
+    fig1.write_html('parallel.html')
+    fig2 = plot_param_importances(study)
+    fig2.write_html('importance.html')
+
+    # Make a table of GPU info
+    g = nvgpu.gpu_info()
+    df = pd.DataFrame.from_dict(g[0], orient="index", columns=["Value"])
+    with open("gpu_info.txt", "w") as outfile:
+        outfile.write(df.to_markdown())
+
+    # run_model_training()
